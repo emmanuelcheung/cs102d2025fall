3a4b18cc7db06ffd7eb1882f6d413d763210a9db
emmanuel.cheung <emmanuel.cheung@cooper.edu>
2025-09-10 20:25:38 -0400

    bingobang2

--- cs102/homework-2/repetitive.c
+++ cs102/homework-2/repetitive.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
-int main( int argc, char
+int main( int argc, char **argv )
+{
+fprintf(stdout,"cs102\n");
+fprintf(stdout,"emmanuel cheung\n");
+fprintf(stdout,"sept 10 2025\n");
+for(int i=0; i<50; i++)
+{
+fprintf(stdout,"a\n");
+fprintf(stdout,"b\n");
+}
+fprintf(stdout,"c\n");
+for(int m=0; m<50; m++)
+{
+fprintf(stdout,"a\n");
+fprintf(stdout,"b\n");
+}
+
+}
